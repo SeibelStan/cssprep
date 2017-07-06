b955fa74960d79eac65c5fdd23b845a27ceb5bbf
SeibelStan <seibel.stan@ya.ru>
2017-07-07 03:32:11 +0600

    Upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Проверка PrepCSS</title>
+	<link rel="stylesheet" href="assets/mixed.min.css">
+</head>
+<body>
+	<span>1</span>
+	<p>2</p>
+</body>
+</html>
